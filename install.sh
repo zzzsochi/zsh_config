74952c20d77c2c0610fe2839ee6509dac70c6331
Alexander Zelenyak <zzz.sochi@gmail.com>
2013-05-10 00:13:25 +0400

    Add "help" key to install.sh and fix print_help function.

--- install.sh
+++ install.sh
@@ -14,15 +14,16 @@ fi
 function print_help() {
     echo "zsh_config installation script"
     echo ""
-    echo "install.sh            install configs from git repository"
-    echo "install.sh install    install configs"
-    echo "install.sh help       print this help"
+    echo "Basic commands:"
+    echo "   install.sh           install configs from git repository"
+    echo "   install.sh install   install configs"
+    echo "   install.sh help      print this help"
     echo ""
     echo "Environment:"
-    echo "ZSH         path to zsh_config installation"
-    echo "            default: $HOME/.zsh"
-    echo "ZSH_REPO    repository for getting zsh_config"
-    echo "            default: $DEFAULT_ZSH_REPO"
+    echo "   ZSH        path to zsh_config installation"
+    echo "                 default: $HOME/.zsh"
+    echo "   ZSH_REPO   repository for getting zsh_config"
+    echo "                 default: $DEFAULT_ZSH_REPO"
 }
 
 
@@ -32,6 +33,7 @@ if [ -z "$1" ]; then
     cd "$ZSH"
     chmod +x install.sh
     ./install.sh install
+    cd -
 
 elif [ "$1" == "install" ]; then
     ln -sf ~/.zsh/zshrc ~/.zshrc
@@ -40,8 +42,15 @@ elif [ "$1" == "install" ]; then
         cp "$ZSH/zprofile-template" "$HOME/.zprofile"
     fi
 
+elif [ "$1" == "help" ]; then
+    print_help
+
+elif [ "$1" == "--help" ]; then
+    print_help
+
 else
     echo "$0: illegal command -- \"$@\""
     echo ""
     print_help
+    return 1
 fi
